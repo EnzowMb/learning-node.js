56d267604d27a814c530afa986009eaa4b0a3807
EnzoMbz <103067639+EnzoMbz@users.noreply.github.com>
2023-07-26 13:03:46 -0300

    modulo5 - aula1 : Verificando parametros digitados

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,20 +1,30 @@
 const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletarLivro } = require("../servicos/livro")
 
 function getLivros(req, res) {
-        try {
-            const livros = getTodosLivros()
-            res.send(livros)
-        } catch (error) {
-            res.status(500) //Caso der erro, retorne o erro 500, que é erro do servidor
-            res.send(error.messsage) //Deve sempre retornar alguma coisa com .send
-        }
+    try {
+        const livros = getTodosLivros()
+        res.send(livros)
+    } catch (error) {
+        res.status(500) //Caso der erro, retorne o erro 500, que é erro do servidor
+        res.send(error.messsage) //Deve sempre retornar alguma coisa com .send
+    }
 }
 
 function getLivro(req, res) {
     try {
         const id = req.params.id //pega os parametros que estão na URL
-        const livro = getLivroPorId(id)
-        res.send(livro)
+
+        //Verifica se tem alguma id && verifica se é um numero ou não:
+        //Number(2) -> 2 -> true
+        // Number("batata") -> NaN -> false
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422) //Algum dado enviado não é consistente com que a aplicação espera
+            res.send("Id inválido")
+        }
+
     } catch (error) {
         res.status(500) //Caso der erro, retorne o erro 500, que é erro do servidor
         res.send(error.messsage) //Deve sempre retornar alguma coisa com .send
@@ -36,13 +46,19 @@ function postLivro(req, res) {
 function patchLivro(req, res) {
     try {
         const id = req.params.id
-        const body = req.body
 
-        modificaLivro(body, id)
-        res.send("Item modificado com sucesso")
+        if (id && Number(id)) {
+            const body = req.body
+
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422) //Algum dado enviado não é consistente com que a aplicação espera
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
-        res.send(error.messsage) 
+        res.send(error.messsage)
     }
 }
 
@@ -50,8 +66,13 @@ function deleteLivro(req, res) {
     try {
         const id = req.params.id
 
-        deletarLivro(id)
-        res.send("Livro deletado com sucesso")
+        if (id && Number(id)) {
+            deletarLivro(id)
+            res.send("Livro deletado com sucesso")
+        } else {
+            res.status(422) //Algum dado enviado não é consistente com que a aplicação espera
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.messsage)
