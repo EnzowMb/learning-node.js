27511fa7c0190fe498028085ff871d596bc47f66
EnzoMbz <103067639+EnzoMbz@users.noreply.github.com>
2023-08-01 00:42:39 -0300

    mudança

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ const port = 8000 //Está dizendo qual vai ser a porta do locahost, o 3000 é o 
 //Com react e o 8000 é o back com Node
 
 app.listen(port, () => { //Escute a porta 8000 porque pode chegar requisições, pedidos de usuarios por lá.
-    console.log(`Escutando a porta ${port}`)
+    console.log(`Escutando porta ${port}`)
 })
